0304d36795a4a051ba8136c11cb85b3def9ccee3
Govind Sharma <govind01sharma@yahoo.in>
2025-03-25 15:24:38 +0530

    Add toast notifications for form validation and authentication; enhance Navbar with logout functionality and links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { axiosInstance } from './lib/axios'
 import { useAuthStore } from './store/useAuthStore'
 import { Loader } from 'lucide-react'
 import { Navigate } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 
 const App = () => {
@@ -37,6 +38,8 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+
+      <Toaster />
     </div>
   )
 }
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -3,6 +3,7 @@ import {MessageSquare, User, Mail, Lock, EyeOff, Eye, Loader2} from 'lucide-reac
 import { Link } from "react-router-dom";
 import { useAuthStore } from '../store/useAuthStore';
 import AuthImagePattern from '../components/AuthImagePattern';
+import toast from 'react-hot-toast';
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -15,11 +16,23 @@ const [formData, setFormData] = useState({
   const { signup, isSigningUp } = useAuthStore();
 
   const validateForm = () => {
+    if (!formData.fullName.trim()) return toast.error("Full name is required");
+    if (!formData.email.trim()) return toast.error("Email is required");
+    if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error("Invalid email format");
+    if (!formData.password) return toast.error("Password is required");
+    if (formData.password.length < 6) return toast.error("Password must be at least 6 characters");
 
+    return true;
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const success = validateForm();
+
+    if (success===true) {
+      signup(formData);
+    }
   };
 
 
