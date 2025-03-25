735c36183bde3fa2fa6ad9330d0020c6e261164b
Govind Sharma <govind01sharma@yahoo.in>
2025-03-25 11:35:03 +0530

    Add CORS support, Axios instance, and authentication state management with Zustand

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import SignUpPage from './pages/SignUpPage'
@@ -6,18 +6,36 @@ import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
+import { axiosInstance } from './lib/axios'
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from 'lucide-react'
+import { Navigate } from 'react-router-dom'
 
 
 const App = () => {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if(isCheckingAuth && !authUser) return (
+    <div className='flex justify-center items-center h-screen'>
+      <Loader className='size-10 animate-spin' />
+    </div>
+  )
+
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+        <Route path="/settings" element={authUser ? <SettingsPage /> : <Navigate to='/login' />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
 
 
       </Routes>
