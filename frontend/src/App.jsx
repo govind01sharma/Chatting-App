5742e1a99c188d891624a84ca9ff667e25dce867
Govind Sharma <govind01sharma@yahoo.in>
2025-03-27 13:33:01 +0530

    Implement online user tracking with Socket.IO; update useAuthStore to manage online users and enhance socket connection logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,9 @@ import { Toaster } from 'react-hot-toast'
 
 
 const App = () => {
-  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  const {authUser, checkAuth, isCheckingAuth, onlineUsers} = useAuthStore();
+
+  console.log({ onlineUsers });
 
   useEffect(() => {
     checkAuth();
